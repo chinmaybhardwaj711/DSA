9c5e675e4dcfdcf31f1a7b04643a69555aee8e21
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-04-08 20:18:25 +0530

    Time: 33 ms (33.45%) | Memory: 162 MB (5.04%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,37 +1,5 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-//         int n = nums.length;
-//         List<Integer> result = new ArrayList<>();
-//         Deque<Integer> dq = new LinkedList<>();
-        
-//         for(int i=0;i<k;i++){
-//             while(dq.size()>0  && nums[dq.getLast()] <nums[i] ){
-//                 dq.removeLast();
-//             }
-//             dq.addLast(i);
-//         }
-
-
-//         for(int i=k;i<n;i++){
-//             result.add(nums[dq.getFirst()]);
-
-//             while(dq.size()>0  && nums[dq.getFirst()] <=i-k ){
-//                 dq.removeFirst();
-//             }
-//             while(dq.size()>0 && nums[dq.getLast()] < nums[i]){
-//                 dq.removeLast();
-//             }
-//             dq.addLast(i);
-//         }
-//         result.add(nums[dq.getFirst()]);
-
-//          int[] res = new int[result.size()];
-//         for(int i=0;i<result.size();i++){
-//             res[i] = result.get(i);
-//         }
-
-// return res;
-
 
     int n = nums.length;
     List<Integer> result = new ArrayList<>();
