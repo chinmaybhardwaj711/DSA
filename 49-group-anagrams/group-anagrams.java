6f2443745d0914e42ce5eb9839dc4d025cdd277c
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-04-17 10:46:15 +0530

    Time: 20 ms (14.29%) | Memory: 50.2 MB (19.00%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,39 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-     HashMap<String,List<String>> map = new HashMap<>();
-     for(String str: strs){
-        char[] chars = str.toCharArray();
-        Arrays.sort(chars);
+    //  HashMap<String,List<String>> map = new HashMap<>();
+    //  for(String str: strs){
+    //     char[] chars = str.toCharArray();
+    //     Arrays.sort(chars);
 
-        String key = new String(chars);
+    //     String key = new String(chars);
 
-        map.putIfAbsent(key,new ArrayList<>());
+    //     map.putIfAbsent(key,new ArrayList<>());
 
-        map.get(key).add(str);
-     }
+    //     map.get(key).add(str);
+    //  }
 
-     return new ArrayList<>(map.values());
+    //  return new ArrayList<>(map.values());
 
+
+//aproach 2 (optimised)
+   HashMap<String,List<String>> map= new HashMap<>();
+   for(String str: strs){
+    int count[] = new int[26];
+
+    for(char ch: str.toCharArray()){
+        count[ch-'a']++;
+    }
+    StringBuilder keyBuilder = new StringBuilder();
+    for(int num:count){
+        keyBuilder.append("#").append(num);
+    }
+    String key = keyBuilder.toString();
+
+    map.putIfAbsent(key,new ArrayList<>());
+    map.get(key).add(str);
+   }
+
+   return new ArrayList<>(map.values());
 
     
     }
