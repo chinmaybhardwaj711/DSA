f97692d18f8f8bce6dfa16b345a26e54699587d8
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-06-10 16:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (49.49%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,48 +1,94 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int piv =-1;
-     for(int i=n-2;i>=0;i--){
-        if(nums[i] < nums[i+1]){
-            piv = i;
-            break;
+      int n = nums.length;
+      int piv = -1;
+       for(int i=n-2;i>=0;i--){
+         if(nums[i] <nums[i+1]){
+           piv = i;
+           break;
+
+          }
         }
-        
-     }
-      if(piv ==-1){
-            int start = 0;int last = n-1;
-            while(start<last){
-                int temp2 = nums[start];
-                nums[start] = nums[last];
-                nums[last] = temp2;
 
-                start++;last--; 
-                
-            }
-            return;
-    }
+      if(piv == -1){
+        //reverse array
+        int start =0;  int last = n-1;
+        while(start<last){
+          int temp = nums[start];
+          nums[start] = nums[last];
+          nums[last] = temp;
+          start++;
+          last--;
+        }
+        return;
+      }
 
-    for(int i=n-1;i>piv;i--){
-        if(nums[i] > nums[piv]){
-            int temp = nums[i];
+
+     for(int i= n-1;i>=0;i--){
+       if(nums[i] > nums[piv]){
+        //swaap
+            int temp2 = nums[i];
             nums[i] = nums[piv];
-            nums[piv] = temp;
+            nums[piv] = temp2; 
             break;
         }
-    }
-    int start = piv+1; int last = n-1;
-    while(start<last){
-       int temp1 = nums[start];
-        nums[start] = nums[last];
-        nums[last] = temp1;
+      }
 
-        start ++;
-        last--;
-    }
+    //step 3
+        int start = piv+1; int end = n-1;
+        while(start<end){
+           int temp = nums[start];
+           nums[start] = nums[end];
+           nums[end] = temp;
 
-
-
-     
-        
-    }
+           start++; end--;
+        }
+    }   
 }
+
+
+
+
+
+
+
+  //     int n = nums.length;
+    //     int piv =-1;
+    //  for(int i=n-2;i>=0;i--){
+    //     if(nums[i] < nums[i+1]){
+    //         piv = i;
+    //         break;
+    //     }
+        
+    //  }
+    //   if(piv ==-1){
+    //         int start = 0;int last = n-1;
+    //         while(start<last){
+    //             int temp2 = nums[start];
+    //             nums[start] = nums[last];
+    //             nums[last] = temp2;
+
+    //             start++;last--; 
+                
+    //         }
+    //         return;
+    // }
+
+    // for(int i=n-1;i>piv;i--){
+    //     if(nums[i] > nums[piv]){
+    //         int temp = nums[i];
+    //         nums[i] = nums[piv];
+    //         nums[piv] = temp;
+    //         break;
+    //     }
+    // }
+    // int start = piv+1; int last = n-1;
+    // while(start<last){
+    //    int temp1 = nums[start];
+    //     nums[start] = nums[last];
+    //     nums[last] = temp1;
+
+    //     start ++;
+    //     last--;
+    // }
+
