efaa5f1b9f63caff4efa924e34ebf9c93c64c50a
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-06-13 12:15:49 +0530

    Time: 29 ms (93.25%) | Memory: 59.2 MB (45.16%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,31 +1,124 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> q = new ArrayList<>();
         int n = nums.length;
         Arrays.sort(nums);
-        for(int i=0;i<n;i++){
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
             if(i>0 && nums[i] == nums[i-1])continue;
-            int j=i+1; int k=n-1;
+            int j =i+1; int k=n-1;
             while(j<k){
-                int sum =nums[i]+ nums[j] + nums[k];
+                int sum = nums[i] + nums[j] + nums[k];
                 if(sum<0){
                     j++;
                 }else if(sum>0){
                     k--;
                 }else{
-                    q.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    ans.add( Arrays.asList(nums[i],nums[j],nums[k]));
+                    
                     j++;
                     k--;
-                     while(j<k && nums[j] == nums[j-1])j++;
-                     while(j<k && nums[k] == nums[k+1])k--; 
+                    while(j<k && nums[j] == nums[j-1])j++;
+                    while(j<k && nums[k] == nums[k+1])k--;
                 }
-               
             }
-
         }
-        return q;
+
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+        // int n = nums.length;
+        // Set<List<Integer>> ans= new HashSet<>();
+        // for(int i=0;i<n;i++){
+        //     HashSet<Integer> set = new HashSet<>();
+        //     for(int j=i+1;j<n;j++){
+        //         int third = -(nums[i] + nums[j]);
+        //         if(set.contains(third)){
+        //             List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
+        //             Collections.sort(temp);
+        //             ans.add(temp);
+        //         }
+
+        //         set.add(nums[j]);
+        //     }
+        // }
+
+        // return new ArrayList<>(ans);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // List<List<Integer>> q = new ArrayList<>();
+        // int n = nums.length;
+        // Arrays.sort(nums);
+        // for(int i=0;i<n;i++){
+        //     if(i>0 && nums[i] == nums[i-1])continue;
+        //     int j=i+1; int k=n-1;
+        //     while(j<k){
+        //         int sum =nums[i]+ nums[j] + nums[k];
+        //         if(sum<0){
+        //             j++;
+        //         }else if(sum>0){
+        //             k--;
+        //         }else{
+        //             q.add(Arrays.asList(nums[i], nums[j], nums[k]));
+        //             j++;
+        //             k--;
+        //              while(j<k && nums[j] == nums[j-1])j++;
+        //              while(j<k && nums[k] == nums[k+1])k--; 
+        //         }
+               
+        //     }
+
+        // }
+        // return q;
+        // // List<List<Integer>> q = new ArrayList<>();
         // Arrays.sort(nums);
         // for(int i=0;i<nums.length;i++){
         //     if(i>0 &&  nums[i] == nums[i-1])continue;
