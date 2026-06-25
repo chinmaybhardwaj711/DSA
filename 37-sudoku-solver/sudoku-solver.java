e001dcaaea0e96b096d7fa349f75f098dbd467c9
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-06-25 13:29:54 +0530

    Time: 216 ms (65.49%) | Memory: 42.8 MB (71.02%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,63 +1,49 @@
 class Solution {
-
-    public boolean isSafe(char[][] board,int row, int col, int number){
-        //horizontal 
-        for(int i=0;i<board[0].length;i++){
-            if(board[row][i] == (char)(number+'0')){
-                return false;
-            }
-        }
-        //verticaly
-        for(int j=0;j<board.length;j++){
-             if(board[j][col] == (char)(number+'0')){
-                return false;
-            }
-        }
-
-        //grid
-        int sr = (row/3)*3;
-        int sc = (col/3)*3;
-
-        for(int i=sr;i<sr+3;i++){
-            for(int j=sc;j<sc+3;j++){
-                if(board[i][j] == (char)(number+'0')){
-                    return false;
-                }
-            }
-        }
-
-        return true;
-
-    }
-    public boolean helper(char[][] board,int row,int col){
-        if(row == 9){
-            return true;
-        }
-        int nxtRow = row; int nxtCol= col+1;
-        if(nxtCol == 9){
-            nxtCol = 0;
-            nxtRow = row+1;
-        }
-
-        if(board[row][col] != '.'){
-            return helper(board,nxtRow,nxtCol);
-        }
-
-        for(int i=1;i<=9;i++){
-            if(isSafe(board,row,col,i)){
-                board[row][col] = (char)(i+'0');
-                if(helper(board,nxtRow,nxtCol)){
-                    return true;
-                }else{
-                    board[row][col] = '.';
-                }
-           
-            }
-        }
-        return false;
+    public boolean isSafe(char board[][], int row, int col,char c){
         
+        //row 
+        for(int i=0;i<9;i++){
+            if(board[row][i] == c)return false;  
+        }
+
+        //col
+        for(int j=0;j<9;j++){
+            if(board[j][col] == c )return false;
+        }
+
+        int nxtrow = 3*(row/3);
+        int nxtcol = 3*(col/3);
+        for(int k=0;k<3;k++){
+            for(int l=0;l<3;l++){
+                if(board[nxtrow+k][nxtcol+l] == c)return false;
+            }
+        }
+        return true;
+    }
+    public boolean helper(char[][]board){
+
+
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] == '.'){
+                    for(char c='1';c<='9';c++){
+                        if(isSafe(board,i,j,c)){
+                            board[i][j] = c;
+
+                            if(helper(board)){
+                                return true;
+                            }
+                            board[i][j] = '.';
+
+                        }
+                    }
+                     return false;
+                }
+            }
+        }
+        return true;
     }
     public void solveSudoku(char[][] board) {
-        helper(board,0,0);
+         helper(board);
     }
 }
