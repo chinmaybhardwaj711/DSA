03ab350ed949a56a7f0f902b0e3a232844a65340
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-07-02 10:55:20 +0530

    Time: 2 ms (71.72%) | Memory: 44.9 MB (60.55%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -21,17 +21,21 @@ class Solution {
         // }
 
 
-        int dp[][] = new int[n+1][2*k+1];
+        // int dp[][] = new int[n+1][2*k+1];
+        int ahead[] = new int[2*k+1];
+
         for(int ind =n-1;ind>=0;ind--){
+             int curr[] = new int[2*k+1];
             for(int transNo=2*k-1;transNo>=0;transNo--){
                 if(transNo%2 ==0){
-                     dp[ind][transNo] = Math.max(-prices[ind] + dp[ind+1][transNo+1] , 0 + dp[ind+1][transNo]);
+                     curr[transNo] = Math.max(-prices[ind] + ahead[transNo+1] , 0 + ahead[transNo]);
                 }else{
-                     dp[ind][transNo] = Math.max(prices[ind] +dp[ind+1][transNo+1] , 0 + dp[ind+1][transNo] );
+                     curr[transNo] = Math.max(prices[ind] +ahead[transNo+1] , 0 + ahead[transNo] );
                 }                    
             }
+            ahead=curr;
         }
-        return dp[0][0];
+        return ahead[0];
 
     }
 }
