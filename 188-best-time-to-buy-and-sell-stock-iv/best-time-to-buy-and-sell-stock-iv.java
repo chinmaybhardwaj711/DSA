4c2ea598cfb2923cb3daa710545568bff85b446c
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-07-02 10:52:58 +0530

    Time: 2 ms (71.72%) | Memory: 44.7 MB (61.29%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int f(int ind,int transNo,int prices[],int n,int k,int dp[][]){
+        if(ind ==n || transNo ==2*k){
+            return  0;
+        }//buy
+
+        if(dp[ind][transNo] != -1)return dp[ind][transNo];
+
+        if(transNo%2 ==0){
+           return dp[ind][transNo] = Math.max(-prices[ind] + f(ind+1,transNo+1,prices,n,k,dp) , 0 + f(ind+1,transNo,prices,n,k,dp));
+        }
+            return dp[ind][transNo] = Math.max(prices[ind] +f(ind+1,transNo+1,prices,n,k,dp) , 0 + f(ind+1,transNo,prices,n,k,dp) );
+
+        
+    }
+    public int maxProfit(int k, int[] prices) {
+        int n =prices.length;
+        // int dp[][] = new int[n][2*k];
+        // for(int row[]:dp){
+        //     Arrays.fill(row,-1);
+        // }
+
+
+        int dp[][] = new int[n+1][2*k+1];
+        for(int ind =n-1;ind>=0;ind--){
+            for(int transNo=2*k-1;transNo>=0;transNo--){
+                if(transNo%2 ==0){
+                     dp[ind][transNo] = Math.max(-prices[ind] + dp[ind+1][transNo+1] , 0 + dp[ind+1][transNo]);
+                }else{
+                     dp[ind][transNo] = Math.max(prices[ind] +dp[ind+1][transNo+1] , 0 + dp[ind+1][transNo] );
+                }                    
+            }
+        }
+        return dp[0][0];
+
+    }
+}
