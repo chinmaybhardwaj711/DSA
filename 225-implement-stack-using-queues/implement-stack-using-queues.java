af87582f66c423415bd3546cb6ec19af88a68f75
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-07-03 08:34:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (72.34%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,70 +1,57 @@
 class MyStack {
-         Queue<Integer> q1 ;
-         Queue<Integer> q2 ;
-
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+  
 
     public MyStack() {
-          q1 = new LinkedList<>();
-          q2 = new LinkedList<>();
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    
     }
     
     public void push(int x) {
-        if(!q1.isEmpty()){
-            q1.add(x);
-        }else{
-            q2.add(x);
-        }
+        q1.add(x);
+        
+    
     }
     
     public int pop() {
-        if(q1.isEmpty() && q2.isEmpty()){
-            return -1;
-        }
-        int top=-1;
+   
+     while(q1.size()>1){
+      q2.add(q1.remove());
+     }
 
-        if(!q1.isEmpty()){
-            while(!q1.isEmpty()){
-                top = q1.remove();
-                if(q1.isEmpty()){
-                    break;
-                }
-                q2.add(top);
-            }
-        }else{
-             while(!q2.isEmpty()){
-                top = q2.remove();
-                if(q2.isEmpty()){
-                    break;
-                }
-                q1.add(top);
-            }
-        }
-        return top;
+     int ans= q1.remove();
+     Queue<Integer> temp  =q2;
+     q2 = q1;
+     q1=temp;
+
+return ans;
+        
     }
     
     public int top() {
-        if(empty()){
-            return -1;
-        }
-        int top =-1;
-        if(!q1.isEmpty()){
-            while(!q1.isEmpty()){
-                top = q1.remove();
-                q2.add(top);
-            }
-            
+     while(q1.size()>1){
+        q2.add(q1.remove());
+     }
+     int ans = q1.peek();
 
-        }else{
-             while(!q2.isEmpty()){
-                top = q2.remove();
-                q1.add(top);
-            }
-        }
-        return top;
+     q2.add(q1.remove());
+
+     Queue<Integer> temp = q2;
+     q2 = q1;
+     q1 = temp;
+
+     return ans;
+        
     }
     
     public boolean empty() {
-        return q1.isEmpty() && q2.isEmpty();
+        if(q1.size()==0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
