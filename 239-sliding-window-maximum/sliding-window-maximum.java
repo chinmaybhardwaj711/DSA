24b6a0400563ff79318b0371ac79d4354359cd54
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-07-07 17:27:47 +0530

    Time: 36 ms (20.92%) | Memory: 160 MB (5.04%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,41 +1,61 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        Deque<Integer> dq = new LinkedList<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k){
+                dq.removeFirst();
+            }
+            while(!dq.isEmpty() && nums[i] >=nums[dq.getLast()]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+            if(i>=k-1){
+                result.add(nums[dq.peekFirst()]);
+            }
 
-    int n = nums.length;
-    List<Integer> result = new ArrayList<>();
-    Deque<Integer> dq = new LinkedList<>();
-    //first window
-    for(int i=0;i<k;i++){
-        while(dq.size()>0 && nums[i] >nums[dq.getLast()]){
-            dq.removeLast();
         }
-        dq.addLast(i);
-    }
-
-    for(int i=k;i<n;i++){
-        result.add(nums[dq.getFirst()]);
-
-        //remvoving el not in window
-        while(dq.size()>0 && dq.getFirst()<=i-k){
-            dq.removeFirst();
+        int res[] = new int[result.size()];
+        for(int i=0;i<result.size();i++){
+            res[i] = result.get(i);
         }
+        return res;
+    // int n = nums.length;
+    // List<Integer> result = new ArrayList<>();
+    // Deque<Integer> dq = new LinkedList<>();
+    // //first window
+    // for(int i=0;i<k;i++){
+    //     while(dq.size()>0 && nums[i] >nums[dq.getLast()]){
+    //         dq.removeLast();
+    //     }
+    //     dq.addLast(i);
+    // }
 
-        while(dq.size()>0 && nums[dq.getLast()] <nums[i]){
-            dq.removeLast();
-        }
-        dq.addLast(i);
-    }
-    result.add(nums[dq.getFirst()]);
+    // for(int i=k;i<n;i++){
+    //     result.add(nums[dq.getFirst()]);
+
+    //     //remvoving el not in window
+    //     while(dq.size()>0 && dq.getFirst()<=i-k){
+    //         dq.removeFirst();
+    //     }
+
+    //     while(dq.size()>0 && nums[dq.getLast()] <nums[i]){
+    //         dq.removeLast();
+    //     }
+    //     dq.addLast(i);
+    // }
+    // result.add(nums[dq.getFirst()]);
 
 
 
 
-     int []res = new int [result.size()];
-    for(int i=0;i<result.size();i++){
-        res[i] = result.get(i);
-    }
+    //  int []res = new int [result.size()];
+    // for(int i=0;i<result.size();i++){
+    //     res[i] = result.get(i);
+    // }
 
-    return res;
+    // return res;
     }
 
    
