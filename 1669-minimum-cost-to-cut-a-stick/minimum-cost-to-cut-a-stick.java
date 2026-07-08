cf15c65c95847c0ca60490d0175e85669337bdc3
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-07-08 19:47:41 +0530

    Time: 21 ms (59.20%) | Memory: 44.6 MB (41.58%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -14,10 +14,10 @@ class Solution {
     public int minCost(int n, int[] cuts) {
         int c = cuts.length;
         ArrayList<Integer> cut = new ArrayList<>();
-        int dp[][] = new int[c+2][c+2];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
-        }
+        // int dp[][] = new int[c+2][c+2];
+        // for(int row[]:dp){
+        //     Arrays.fill(row,-1);
+        // }
         for(int x:cuts){
             cut.add(x);
         }
@@ -26,7 +26,23 @@ class Solution {
         Collections.sort(cut);
         // int c = cut.size();
 
-        return f(1,c,cut,dp);
+        int dp[][] = new int[c+2][c+2];
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j)continue;
+                int min = Integer.MAX_VALUE;
+                for(int ind=i;ind<=j;ind++){
+                    int cost = cut.get(j+1) - cut.get(i-1) + dp[i][ind-1] + dp[ind+1][j];
+                    min = Math.min(cost,min);
+                }
+             dp[i][j] =  min;
+            }
+        }
+
+
+
+
+        return dp[1][c];
 
     }
 }
