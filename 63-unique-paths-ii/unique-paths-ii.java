7aff950e708aa52789e1ddfdf4d3e67b8321e36b
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-07-13 22:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (80.98%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int f(int i,int j,int[][] obstacleGrid,int dp[][]){
+         if(i>=0 && j>=0 && obstacleGrid[i][j] ==1){
+            return 0;
+        }
         if(i==0&& j==0){
             return 1;
         }
-        if(i>=0 && j>=0 && obstacleGrid[i][j] ==1){
-            return 0;
-        }
+ 
         if(i<0 || j<0){
             return 0;
         }
@@ -20,36 +21,37 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n = obstacleGrid.length;
         int m = obstacleGrid[0].length;
-        // int dp[][] = new int[n][m];
-        // for(int row[]: dp){
-        //     Arrays.fill(row,-1);
-        // }
-
         int dp[][] = new int[n][m];
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i>=0 &&j>=0 && obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if(i ==0 && j==0 ){
-                    dp[i][j] = 1;
-                    continue;
-                }
-                int up=0;
-                int left =0;
-                if(i>0){
-                     up = dp[i-1][j];
-                }
-                if(j>0){
-                     left = dp[i][j-1];
-                }
-                dp[i][j] = up+left;
-
-            }
-
+        for(int row[]: dp){
+            Arrays.fill(row,-1);
         }
-        return dp[n-1][m-1];
+        return f(n-1,m-1,obstacleGrid,dp);
+
+        // int dp[][] = new int[n][m];
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(i>=0 &&j>=0 && obstacleGrid[i][j] == 1){
+        //             dp[i][j] = 0;
+        //             continue;
+        //         }
+        //         if(i ==0 && j==0 ){
+        //             dp[i][j] = 1;
+        //             continue;
+        //         }
+        //         int up=0;
+        //         int left =0;
+        //         if(i>0){
+        //              up = dp[i-1][j];
+        //         }
+        //         if(j>0){
+        //              left = dp[i][j-1];
+        //         }
+        //         dp[i][j] = up+left;
+
+        //     }
+
+        // }
+        // return dp[n-1][m-1];
     }
 }
