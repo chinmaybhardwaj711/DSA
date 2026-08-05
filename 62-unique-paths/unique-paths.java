b5c9846061987e9adf1ef08f18f5cf1b34366c05
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-05 09:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.24%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,35 +1,92 @@
 class Solution {
-    public int uniqueUtil(int i,int j,int[][]dp){
-      if(i==0||j==0)return 1;
-        if(i<0 || j<0)return 0;
 
-        if(dp[i][j] !=-1){
+
+
+
+    public int f(int i,int j, int dp[][]){
+        if(i==0 && j==0){
+            return 1;
+        }
+        if(i<0 || j<0){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
+        int up = f(i-1,j,dp);
+        int left = f(i,j-1,dp);
 
-                //up
+        return dp[i][j] =up+left;
+    }
+    
+    // public int uniqueUtil(int i,int j,int[][]dp){
+    //  if(i==0||j==0)return 1;
+    //     if(i<0 || j<0)return 0;
+
+    //     if(dp[i][j] !=-1){
+    //         return dp[i][j];
+    //     }
+
+    //             //up
             
                 
-        int up = uniqueUtil(i-1,j,dp);
+    //     int up = uniqueUtil(i-1,j,dp);
 
-        int left = uniqueUtil(i,j-1,dp);        
+    //     int left = uniqueUtil(i,j-1,dp);        
               
 
-        return  dp[i][j] =up+left;
+    //     return  dp[i][j] =up+left;
 
         
         
-    }
+    // }
     public int uniquePaths(int m, int n) {
 
-        int ans = 0;
         int dp[][] = new int[m][n];
-        for(int row[]:dp){
-             Arrays.fill(row,-1);
+        for(int row[]: dp){
+            Arrays.fill(row,-1);
         }
-       
-        ans = uniqueUtil(m-1,n-1,dp);
+        int ans = f(m-1,n-1,dp);
+
         return ans;
 
+
+    //     int ans = 0;
+    //     int dp[][] = new int[m][n];
+    //     for(int row[]:dp){
+    //          Arrays.fill(row,-1);
+    //     }
+       
+    //     ans = uniqueUtil(m-1,n-1,dp);
+    //     return ans;
+
     }
+
+    // public int uniquePaths(int m, int n){
+    //     int dp[][] = new int[m][n];
+    //     for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             if(i==0 && j==0){
+    //                 dp[i][j]=1;
+    //                 continue;
+    //             }else{
+    //                 int up =0;int left=0;
+    //                 if(i>0){
+    //                      up = dp[i-1][j];
+    //                     }   
+    //                 if(j>0){
+    //                     left = dp[i][j-1];
+    //                 }
+    //                 dp[i][j] =up+left;
+    //             }
+                
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
+    // }
+
+
+
+    
 }
