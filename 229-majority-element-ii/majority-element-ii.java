805f77e99f5efebb70388565f3ba5d7993cae96f
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-05 09:24:12 +0530

    Time: 3 ms (72.18%) | Memory: 52 MB (86.41%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,46 +1,89 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int cnt1 =0;
-        int cnt2 = 0;
-        int el1 = Integer.MIN_VALUE;
-        int el2 = Integer.MIN_VALUE;
+        // int cnt1 =0;
+        // int cnt2 = 0;
+        // int el1 = Integer.MIN_VALUE;
+        // int el2 = Integer.MIN_VALUE;
 
-        for(int i=0;i<nums.length;i++){
-            if(cnt1 ==0 && nums[i] != el2 ){
-                el1 = nums[i];
-                cnt1 =1;
-            }else if(cnt2 == 0 && nums[i] !=el1){
-                el2 =nums[i];
-                cnt2 =1;
-            }else if(nums[i] == el1){
-                cnt1++;
-            }else if(nums[i] == el2){
-                cnt2++;
-            }else{
-                cnt1--;
-                cnt2--;
-            }
+        // for(int i=0;i<nums.length;i++){
+        //     if(cnt1 ==0 && nums[i] != el2 ){
+        //         el1 = nums[i];
+        //         cnt1 =1;
+        //     }else if(cnt2 == 0 && nums[i] !=el1){
+        //         el2 =nums[i];
+        //         cnt2 =1;
+        //     }else if(nums[i] == el1){
+        //         cnt1++;
+        //     }else if(nums[i] == el2){
+        //         cnt2++;
+        //     }else{
+        //         cnt1--;
+        //         cnt2--;
+        //     }
 
            
-        }
-         cnt1 = 0;
-         cnt2 =0;
-        List<Integer> result = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == el1)cnt1++;
-            if(nums[i] == el2)cnt2++;
-        }
-        int min = (int)nums.length/3+1;
-        if(cnt1>=min){
-            result.add(el1);
-        }
-         if(cnt2>=min && el1!=el2){
-            result.add(el2);
-        }
+        // }
+        //  cnt1 = 0;
+        //  cnt2 =0;
+        // List<Integer> result = new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] == el1)cnt1++;
+        //     else if(nums[i] == el2)cnt2++;
+        // }
+        // int min = (int)nums.length/3+1;
+        // if(cnt1>=min){
+        //     result.add(el1);
+        // }
+        //  if(cnt2>=min && el1!=el2){
+        //     result.add(el2);
+        // }
 
-        return result;
+        // return result;
         
 
+    int el1 = Integer.MIN_VALUE;
+    int el2 = Integer.MIN_VALUE;
+
+    int cnt1 =0;
+    int n =nums.length;
+    int cnt2 =0;
+    for(int i=0;i<n;i++){
+        if(cnt1 == 0 && el2!=nums[i]){
+            el1 = nums[i];
+        }
+        if(cnt2 ==0 && el1 != nums[i]){
+            el2 = nums[i];
+        }
+
+        if(nums[i] == el1){
+            cnt1++;
+        }else if(nums[i] == el2){
+            cnt2++;
+        }else{
+            cnt1--;
+            cnt2--;
+        }
+
+    }
+
+    List<Integer> ans = new ArrayList<>();
+ cnt1 =0; cnt2 =0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i] == el1){
+            cnt1++;
+        }else if(nums[i] == el2){
+            cnt2++;
+        }
+
+    }
+    int min = n/3+1;
+    if(cnt1 >= min){
+        ans.add(el1);
+    }
+    if(cnt2>=min && el1 !=el2){
+        ans.add(el2);
+    }
+    return ans;
 
 
 
