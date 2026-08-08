f344356bb4c61c7be37659dd05c78107bbc994e7
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-08 10:24:08 +0530

    Time: 6 ms (74.50%) | Memory: 47.7 MB (14.43%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,24 +1,49 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // int left =0;
+        // int right =0;
+        // int maxLen = 0;
+        // int n = s.length();
+        // int Hash[] = new int[256];
+        // Arrays.fill(Hash,-1);
+
+        // while(right<n){
+        //     char ch = s.charAt(right);
+        //     if(Hash[ch] != -1){
+        //         if(Hash[ch] >= left){
+        //            left = Hash[ch] +1;
+        //         }
+        //     }
+        //     int len = right-left+1;
+        //     maxLen = Math.max(len,maxLen);
+
+        //     Hash[ch]= right;
+
+
+        // }
+        // return maxLen;
+
+
+        int []Hash = new int[256];
         int left =0;
-        int right =0;
+        Arrays.fill(Hash,-1);
         int n = s.length();
-        int maxLen = 0;
-       int[] Hash = new int[256];
-       Arrays.fill(Hash,-1);
-       while(right<n){
-         if(Hash[s.charAt(right)] != -1){
-            if(Hash[s.charAt(right)] >=left){
-                left = Hash[s.charAt(right)] +1;
+        int maxLen =0;
+        int right = 0;
+        while(right<n){
+            char ch = s.charAt(right);
+            if(Hash[ch] != -1){
+                if(Hash[ch] >=left){
+                    left = Hash[ch] +1;
+                }
             }
-         }
-         int len = right-left+1;
-             maxLen = Math.max(len,maxLen);
-            Hash[s.charAt(right)] = right;
+            int len = right-left+1;
+            maxLen = Math.max(maxLen,len);
+            Hash[ch] = right;
             right++;
-        
         }
-       return maxLen;
+        return maxLen;
+
 
     }
 }
