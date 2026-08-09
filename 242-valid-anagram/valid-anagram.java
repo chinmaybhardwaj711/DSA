2512feaec31eb75f519c4a8baa5c1cd3c7f3b6c9
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-09 16:07:32 +0530

    Time: 5 ms (61.62%) | Memory: 46.7 MB (9.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,56 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length()){
+    //     if(s.length() != t.length()){
+    //         return false;
+    //     }
+    //   char s2[] = s.toCharArray();
+    //   char t2[] = t.toCharArray();
+
+    //   Arrays.sort(s2);
+    //   Arrays.sort(t2);
+
+    //   return Arrays.equals(s2, t2);
+
+
+
+
+
+
+
+
+
+
+        char char1[] = s.toCharArray();
+        char char2[] = t.toCharArray();
+
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+
+        if(Arrays.equals(char1,char2)){
+            return true;
+        }else {
             return false;
         }
-      char s2[] = s.toCharArray();
-      char t2[] = t.toCharArray();
+        // for(int i=0;i<s.length();i++){
+        //       if(char1[i] != char2[i]){
+        //     return false;
+        //     }
+        // }
 
-      Arrays.sort(s2);
-      Arrays.sort(t2);
 
-      return Arrays.equals(s2, t2);
+
+       
+    
+      
+
+
+
+
+
+
+
+
 
     }
 }
