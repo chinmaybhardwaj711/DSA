e55e27c4245962d6b2fb87524f2e8def759db418
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-23 09:25:52 +0530

    Time: 48 ms (42.71%) | Memory: 116 MB (9.09%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,18 +1,41 @@
-class Solution {
+// class Solution {
+//     public int eraseOverlapIntervals(int[][] intervals) {
+//         int n = intervals.length;
+//         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+//         int start = intervals[0][0];
+//         int cnt =0;
+//         int end = intervals[0][1];
+//         for(int i=1;i<n;i++){
+//             if(end <=intervals[i][0] || start >=intervals[i][1]){
+//                 end = intervals[i][1];
+//             }else{
+//                 cnt++;
+//                 end = Math.min(end,intervals[i][1]);
+//             }
+//         }
+//         return cnt;
+//     }
+// }class Solution {
+   class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         int n = intervals.length;
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        int start = intervals[0][0];
-        int cnt =0;
         int end = intervals[0][1];
-        for(int i=1;i<n;i++){
-            if(end <=intervals[i][0] || start >=intervals[i][1]){
+        int i=1;int cnt =0;
+        while(i<n){
+            if(end<=intervals[i][0]){
                 end = intervals[i][1];
+                i++;
+                
             }else{
                 cnt++;
-                end = Math.min(end,intervals[i][1]);
+                 end = Math.min(end,intervals[i][1]);
+                 i++;
             }
+        
         }
         return cnt;
+
+            
     }
 }
