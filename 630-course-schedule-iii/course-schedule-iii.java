3718722aee1ba1d4995151ab9bf77373bbfb67a3
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-25 09:18:34 +0530

    Time: 39 ms (56.23%) | Memory: 57.5 MB (44.64%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.java
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses,(a,b)->a[1]-b[1]);
+        Arrays.sort(courses,(a,b)->Integer.compare(a[1],b[1]));
 
-        long time =0;
-        int i=0;
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Integer.compare(b,a));
-        while(i<courses.length){
-            int duration = courses[i][0];
+
+        int i=0;
+        int n = courses.length;
+        int time =0;
+        while(i<n){
             int deadline = courses[i][1];
-
+            int duration = courses[i][0];
+          
             time+=duration;
-            pq.offer(duration);
-        
-
-            if(time>deadline){
-                int longest = pq.poll();
-                time-=longest;
-            }
+            pq.offer(courses[i][0]);
             i++;
+
+              if(time > deadline){
+               int remove = pq.poll();
+                time -=remove;
+
+            }
         }
         return pq.size();
+
     }
 }
