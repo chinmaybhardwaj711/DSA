7d55cf2bb0cb43bc13ef0dbd823367edf698960f
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-29 21:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (53.24%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -20,15 +20,31 @@ class Solution {
     public void flatten(TreeNode root) {
        
 
+        // if(root == null){
+        //     return;
+        // }
+        // flatten(root.right);
+        // flatten(root.left);
+
+        // root.right = prev;
+        // root.left = null;
+        // prev = root;
+
+
+
         if(root == null){
-            return;
+            return ;
         }
+
         flatten(root.right);
         flatten(root.left);
 
         root.right = prev;
         root.left = null;
+
+
         prev = root;
+        
 
     }
 }
