ae3a54d6381baea4bd8ef758909668c4aeab383b
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-29 14:56:50 +0530

    Time: 16 ms (52.67%) | Memory: 70 MB (35.20%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,3 +1,40 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode(int x) { val = x; }
+//  * }
+//  */
+// class Solution {
+
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null){
+//             return null;
+//         }
+//         if(root == p || root == q){
+//             return root;
+//         }
+//         TreeNode left = lowestCommonAncestor(root.left,p,q);
+//         TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+//          if(left!=null &&  right!=null){
+//             return root;
+//          }
+//          if(left!=null && right==null){
+//             return left;
+//          }
+
+//         if(right!=null && left==null){
+//             return right;
+//         }
+//         if(right==null && left==null){
+//             return null ;
+//         } 
+//         return  null;      
+//     }
+// }
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -8,30 +45,26 @@
  * }
  */
 class Solution {
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null){
             return null;
         }
-        if(root == p || root == q){
+        if( root == p || root == q){
             return root;
         }
+
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
 
-         if(left!=null &&  right!=null){
+        if(left!= null && right != null){
             return root;
-         }
-         if(left!=null && right==null){
+        }
+        if(left!= null && right == null){
             return left;
-         }
-
-        if(right!=null && left==null){
+        }
+        if(left== null && right!= null ){
             return right;
         }
-        if(right==null && left==null){
-            return null ;
-        } 
-        return  null;      
+        return null;
     }
 }
