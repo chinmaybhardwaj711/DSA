e2907e3e17cde908ba19f293102a854bad1ba188
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-29 16:05:58 +0530

    Time: 1 ms (81.28%) | Memory: 43.5 MB (90.70%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,53 +1,97 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public void f(TreeNode root,List<List<Integer>> ans){
-        if(root == null){
-            return ;
-        }
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public void f(TreeNode root,List<List<Integer>> ans){
+//         if(root == null){
+//             return ;
+//         }
         
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        boolean lefttoRight = true;
-        while(!q.isEmpty()){
-            int size = q.size();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+//         boolean lefttoRight = true;
+//         while(!q.isEmpty()){
+//             int size = q.size();
 
+//             List<Integer> level = new ArrayList<>();
+//             for(int i=0;i<size;i++){
+//                 level.add(0);
+//             }
+//             for(int i=0;i<size;i++){
+//                 TreeNode curr = q.remove();
+//                int index = (lefttoRight)? i:size-i-1;
+
+//                level.set(index,curr.val);
+//                 if(curr.left!= null){
+//                     q.add(curr.left);
+//                 }
+//                 if(curr.right != null){
+//                     q.add(curr.right);
+//                 }
+
+//             }
+//             lefttoRight = !lefttoRight;
+//             ans.add(level);
+//         }
+//     }
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         f(root,ans);
+//         return ans;
+
+//     }
+// }
+
+class Solution {
+   
+    public void f(TreeNode root,List<List<Integer>>ans){
+        if( root == null){
+            return;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+        boolean ltor = true;
+
+        while(!q.isEmpty()){
+         
+            int size = q.size();
             List<Integer> level = new ArrayList<>();
             for(int i=0;i<size;i++){
                 level.add(0);
             }
-            for(int i=0;i<size;i++){
-                TreeNode curr = q.remove();
-               int index = (lefttoRight)? i:size-i-1;
 
-               level.set(index,curr.val);
-                if(curr.left!= null){
-                    q.add(curr.left);
+            for(int i=0;i<size;i++){
+                 TreeNode curr = q.remove();
+                int index = (ltor)?i:size-1-i;
+                level.set(index,curr.val);
+
+                if(curr.left != null){
+                    q.offer(curr.left);
                 }
                 if(curr.right != null){
-                    q.add(curr.right);
+                    q.offer(curr.right);
                 }
-
             }
-            lefttoRight = !lefttoRight;
+            ltor = !ltor;
             ans.add(level);
         }
     }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
         List<List<Integer>> ans = new ArrayList<>();
         f(root,ans);
         return ans;
