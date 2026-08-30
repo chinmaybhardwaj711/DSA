3e2cf873bbe42de3836c89b24ddd63aa397a68f3
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-30 16:25:32 +0530

    Time: 6 ms (96.88%) | Memory: 47.4 MB (94.38%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,3 +1,33 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode(int x) { val = x; }
+//  * }
+//  */
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         TreeNode curr = root;
+
+//         while(curr !=null){
+//             if(curr.val > p.val && curr.val > q.val){
+//                 curr = curr.left;
+//             }else if(curr.val <p.val && curr.val <q.val){
+//                 curr = curr.right;
+//             }else{
+//                 return curr;
+//             }
+//         }
+//         return null;
+        
+//     }
+// }
+
+
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -10,18 +40,19 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode curr = root;
+     
 
-        while(curr !=null){
-            if(curr.val > p.val && curr.val > q.val){
-                curr = curr.left;
-            }else if(curr.val <p.val && curr.val <q.val){
-                curr = curr.right;
-            }else{
-                return curr;
-            }
+      while(root!=null){
+        if(root.val<p.val && root.val <q.val){
+            root = root.right;
+        }else if(root.val >p.val && root.val >q.val){
+            root = root.left;
+        }else{
+            return root;
         }
+
+      }
         return null;
-        
+    
     }
 }
