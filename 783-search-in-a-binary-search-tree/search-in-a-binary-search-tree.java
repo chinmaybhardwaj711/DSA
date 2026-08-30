ab3c8c7a45df9e6a59fcc171fad7b0ff4b32137b
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-30 13:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (16.21%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -1,3 +1,36 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public TreeNode searchBST(TreeNode root, int val) {
+//         if(root == null){
+//             return  null;
+//         }
+//         if(root.val == val){
+//             return root;
+//         }
+
+//         if(root.val<val){
+//             return searchBST(root.right,val);
+//         }else{
+//            return  searchBST(root.left,val);
+//         }
+        
+//     }
+
+// }
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,19 +48,16 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null){
-            return  null;
+        while(root != null){
+            if(root.val== val){
+                return root;
+            }else if(val<root.val){
+                root = root.left;
+            }else{
+                root = root.right;
+            }
         }
-        if(root.val == val){
-            return root;
-        }
-
-        if(root.val<val){
-            return searchBST(root.right,val);
-        }else{
-           return  searchBST(root.left,val);
-        }
-        
+         return null;
+     
     }
-
 }
