ad446d12686c281e0d4bd8476ccc2d75c0262329
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-31 14:27:26 +0530

    Time: 3 ms (72.90%) | Memory: 46.9 MB (92.97%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,51 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//      HashSet<Integer> map = new HashSet<>();
+//     public boolean findTarget(TreeNode root, int k) {
+//         TreeNode node = root;
+//         if(node == null){
+//             return false;
+//         }
+//         if(map.contains(k-node.val)){
+//             return true;
+//         }
+//         map.add(node.val);
+
+//         return findTarget(node.left,k) || findTarget(node.right,k);
+//     }
+
+    
+
+
+// }
+
+class Solution {
+    HashSet<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null){
+            return false;
+        }
+        if(set.contains(k-root.val)){
+            return true;
+        }
+        set.add(root.val);
+
+      boolean left = findTarget(root.left,k);
+      boolean right = findTarget(root.right,k);return left ||right;
+        
+    }
+}
