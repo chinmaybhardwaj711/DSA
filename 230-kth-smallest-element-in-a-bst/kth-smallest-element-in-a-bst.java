08ed3b624f9eda32351ec3c1883480a475e540c8
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-08-31 13:19:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (68.88%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -1,3 +1,41 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     int cnt =0;
+//     TreeNode ans = null;
+//     public int kthSmallest(TreeNode root, int k) {
+//         TreeNode node = root;
+//         helper(node,k);
+//         return ans.val;
+//     }
+//     public void helper(TreeNode node,int k){
+//         if(node == null){
+//             return;
+//         }
+//         helper(node.left,k);
+//         cnt++;
+//         if(cnt == k){
+//             ans = node;
+//             return;
+//         }
+//         helper(node.right,k);
+
+//     }
+// }\
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -16,22 +54,25 @@
 class Solution {
     int cnt =0;
     TreeNode ans = null;
-    public int kthSmallest(TreeNode root, int k) {
-        TreeNode node = root;
-        helper(node,k);
-        return ans.val;
-    }
-    public void helper(TreeNode node,int k){
-        if(node == null){
+    public void helper(TreeNode root,int k){
+        if(root == null){
             return;
         }
-        helper(node.left,k);
-        cnt++;
-        if(cnt == k){
-            ans = node;
-            return;
-        }
-        helper(node.right,k);
 
+        helper(root.left,k);
+        cnt++;
+        if(cnt ==k){
+            ans = root;
+            return;
+        }
+        helper(root.right,k);
     }
+
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root,k);
+        return ans.val;
+
+         
+    }
+
 }
