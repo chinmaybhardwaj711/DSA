5cbc363fd38f1eb66ba378f55862be30abe5e591
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-02 07:52:02 +0530

    Time: 13 ms (47.59%) | Memory: 44.1 MB (62.78%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,23 +1,85 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode(int x) { val = x; }
+//  * }
+//  */
+// class Solution {
+//     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+//         HashMap<TreeNode,TreeNode> parent = new HashMap<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//     List<Integer> ans = new ArrayList<>();
+//         q.offer(root);
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             for(int i=0;i<size;i++){
+//                 TreeNode curr = q.poll();
+
+//                 if(curr.left!=null){
+//                     parent.put(curr.left,curr);
+//                     q.offer(curr.left);
+//                 }
+//                 if(curr.right!=null){
+//                     parent.put(curr.right,curr);
+//                     q.offer(curr.right);
+//                 }
+//             }
+//         }
+
+//         Queue<TreeNode> q2 = new LinkedList<>();
+//         Set<TreeNode> visited = new HashSet<>();
+
+//         q2.offer(target);
+//         visited.add(target);
+//         while(k>0 && !q2.isEmpty()){
+//             int size = q2.size();
+//             for(int i=0;i<size;i++){
+//                 TreeNode temp = q2.poll();
+
+//                 if(temp.left!=null && !visited.contains(temp.left)){
+//                     q2.offer(temp.left);
+//                     visited.add(temp.left);
+//                 }
+  
+//                 if(temp.right!=null && !visited.contains(temp.right)){
+//                     q2.offer(temp.right);
+//                     visited.add(temp.right);
+//                 }     
+//                 if(parent.containsKey(temp) && !visited.contains(parent.get(temp))){
+//                     q2.offer(parent.get(temp));
+//                     visited.add(parent.get(temp));
+//                 }       
+
+//             }
+//             k--;
+//         }
+//         while(!q2.isEmpty()){
+//             ans.add(q2.poll().val);
+//         }
+//         return ans;
+
+      
+
+//     }
+// }
+
+
+
+
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         HashMap<TreeNode,TreeNode> parent = new HashMap<>();
         Queue<TreeNode> q = new LinkedList<>();
-    List<Integer> ans = new ArrayList<>();
+
         q.offer(root);
+
         while(!q.isEmpty()){
             int size = q.size();
             for(int i=0;i<size;i++){
                 TreeNode curr = q.poll();
-
                 if(curr.left!=null){
                     parent.put(curr.left,curr);
                     q.offer(curr.left);
@@ -25,43 +87,48 @@ class Solution {
                 if(curr.right!=null){
                     parent.put(curr.right,curr);
                     q.offer(curr.right);
-                }
+                }              
             }
         }
 
+
         Queue<TreeNode> q2 = new LinkedList<>();
-        Set<TreeNode> visited = new HashSet<>();
+       HashSet<TreeNode> visited = new HashSet<>();
 
         q2.offer(target);
         visited.add(target);
-        while(k>0 && !q2.isEmpty()){
+        while( k>0 && !q2.isEmpty()){
             int size = q2.size();
             for(int i=0;i<size;i++){
-                TreeNode temp = q2.poll();
-
-                if(temp.left!=null && !visited.contains(temp.left)){
-                    q2.offer(temp.left);
-                    visited.add(temp.left);
+                TreeNode curr = q2.poll();
+                if(curr.left!=null && !visited.contains(curr.left)){
+                    q2.offer(curr.left);
+                    visited.add(curr.left);
                 }
-  
-                if(temp.right!=null && !visited.contains(temp.right)){
-                    q2.offer(temp.right);
-                    visited.add(temp.right);
-                }     
-                if(parent.containsKey(temp) && !visited.contains(parent.get(temp))){
-                    q2.offer(parent.get(temp));
-                    visited.add(parent.get(temp));
-                }       
-
+               if(curr.right!=null && !visited.contains(curr.right)){
+                    q2.offer(curr.right);
+                    visited.add(curr.right);             
+                }
+                if(parent.containsKey(curr) && !visited.contains(parent.get(curr))){
+                    q2.offer(parent.get(curr));
+                    visited.add(parent.get(curr));
+                }
             }
-            k--;
+            k--;    
         }
+        List<Integer> ans = new ArrayList<>();
         while(!q2.isEmpty()){
             ans.add(q2.poll().val);
         }
-        return ans;
 
+        return ans;
       
 
     }
 }
+
+
+
+
+
+
