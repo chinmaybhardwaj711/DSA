0b5e0d47fe8fc0e561df6b4f0964b8517d0c6eca
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-03 08:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (25.58%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
@@ -1,3 +1,101 @@
+// // // /**
+// // //  * Definition for a binary tree node.
+// // //  * public class TreeNode {
+// // //  *     int val;
+// // //  *     TreeNode left;
+// // //  *     TreeNode right;
+// // //  *     TreeNode() {}
+// // //  *     TreeNode(int val) { this.val = val; }
+// // //  *     TreeNode(int val, TreeNode left, TreeNode right) {
+// // //  *         this.val = val;
+// // //  *         this.left = left;
+// // //  *         this.right = right;
+// // //  *     }
+// // //  * }
+// // //  */
+// // // class Solution {
+// // //     public TreeNode build(int nums[], int start,int end){
+// // //         if(start>end){
+// // //             return null;
+// // //         }
+// // //         int mid = (start+end)/2;
+// // //         TreeNode root = new TreeNode(nums[mid]);
+
+// // //         root.left = build(nums,start,mid-1);
+// // //         root.right = build(nums,mid+1,end);
+
+// // //         return root;
+
+// // //     }
+// // //     public TreeNode sortedArrayToBST(int[] nums) {
+// // //         return build(nums,0,nums.length-1);
+// // //     }
+// // // }
+
+
+// // /**
+// //  * Definition for a binary tree node.
+// //  * public class TreeNode {
+// //  *     int val;
+// //  *     TreeNode left;
+// //  *     TreeNode right;
+// //  *     TreeNode() {}
+// //  *     TreeNode(int val) { this.val = val; }
+// //  *     TreeNode(int val, TreeNode left, TreeNode right) {
+// //  *         this.val = val;
+// //  *         this.left = left;
+// //  *         this.right = right;
+// //  *     }
+// //  * }
+// //  */
+// // class Solution {
+// //     public TreeNode build(int nums[], int left,int right){
+// //         if(left>right){
+// //             return null;
+// //         }
+// //             int mid = left+(right-left)/2;
+// //             TreeNode root = new TreeNode(nums[mid]);
+// //             root.left = build(nums,left,mid-1);
+// //             root.right = build(nums,mid+1,right);
+        
+// //         return root;
+// //     }
+// //     public TreeNode sortedArrayToBST(int[] nums) {
+// //        return  build(nums,0,nums.length-1);
+        
+// //     }
+// // }\/**
+// //  * Definition for a binary tree node.
+// //  * public class TreeNode {
+// //  *     int val;
+// //  *     TreeNode left;
+// //  *     TreeNode right;
+// //  *     TreeNode() {}
+// //  *     TreeNode(int val) { this.val = val; }
+// //  *     TreeNode(int val, TreeNode left, TreeNode right) {
+// //  *         this.val = val;
+// //  *         this.left = left;
+// //  *         this.right = right;
+// //  *     }
+// //  * }
+// //  */
+// class Solution {
+//     public TreeNode build(int []nums,int start,int end){
+//       if(start>end){
+//         return null;
+//       }
+       
+//             int mid = (start+end)/2;
+//             TreeNode root = new TreeNode(nums[mid]);
+//             root.left = build(nums,start,mid-1);
+//             root.right = build(nums,mid+1,end);
+//         return root;
+//     }
+//     public TreeNode sortedArrayToBST(int[] nums) {
+//        return build(nums,0,nums.length-1);
+        
+//     }
+// }
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -18,14 +116,12 @@ class Solution {
         if(start>end){
             return null;
         }
-        int mid = (start+end)/2;
-        TreeNode root = new TreeNode(nums[mid]);
 
+        int mid = start+ (end-start)/2;
+        TreeNode root = new TreeNode(nums[mid]);
         root.left = build(nums,start,mid-1);
         root.right = build(nums,mid+1,end);
-
         return root;
-
     }
     public TreeNode sortedArrayToBST(int[] nums) {
         return build(nums,0,nums.length-1);
