9b3234a2fa2df0c8f78dbce05be6aab7baef633d
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-03 22:44:21 +0530

    Time: 78 ms (18.09%) | Memory: 72.7 MB (98.38%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,29 +1,47 @@
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+//         // for(int num:nums){
+//         //     pq.add(num);
+//         // }
+//         // int ans =0;
+//         // while(k>0){
+//         //     ans =pq.remove();
+//         //     k--;
+//         // }
+//         // return ans;
+
+
+
+
+//         //optimal
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         for(int num:nums){
+//             pq.offer(num);
+
+//             if(pq.size()>k){
+//                 pq.poll();
+//             }
+
+//         }
+//         return pq.peek();
+//     }
+// }
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-        // for(int num:nums){
-        //     pq.add(num);
-        // }
-        // int ans =0;
-        // while(k>0){
-        //     ans =pq.remove();
-        //     k--;
-        // }
-        // return ans;
-
-
-
-
-        //optimal
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
         for(int num:nums){
-            pq.offer(num);
-
-            if(pq.size()>k){
-                pq.poll();
-            }
-
+            pq.add(num);
         }
-        return pq.peek();
+    int ans =0;
+        while(k>0){
+            
+             ans =pq.remove();
+            k--;
+            if(k==0){
+                return ans;
+            }
+        }
+        return ans;
     }
 }
