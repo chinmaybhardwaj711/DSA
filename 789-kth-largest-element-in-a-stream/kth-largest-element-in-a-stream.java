1c3592a1ab3c08f9f8417e16e05973c1117f13c6
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-04 14:22:51 +0530

    Time: 21 ms (94.20%) | Memory: 52.1 MB (57.02%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,25 +1,54 @@
+// class KthLargest {
+//     private PriorityQueue<Integer> minHeap =new PriorityQueue<>();
+//     private int k;
+//     public KthLargest(int k, int[] nums) {
+//         this.k = k;
+//         for(int num:nums){
+//             minHeap.offer(num);
+
+//             if(minHeap.size() >k){
+//                 minHeap.poll();
+//             }
+//         }
+//     }
+    
+//     public int add(int val) {
+//         minHeap.offer(val);
+
+//         if(minHeap.size() >k){
+//             minHeap.poll();
+//         }
+
+//         return minHeap.peek();
+//     }
+// }
+
+// /**
+//  * Your KthLargest object will be instantiated and called as such:
+//  * KthLargest obj = new KthLargest(k, nums);
+//  * int param_1 = obj.add(val);
+//  */
 class KthLargest {
-    private PriorityQueue<Integer> minHeap =new PriorityQueue<>();
-    private int k;
+    public int k;
+    public PriorityQueue<Integer> pq ;
     public KthLargest(int k, int[] nums) {
         this.k = k;
+        pq  = new PriorityQueue<>();
         for(int num:nums){
-            minHeap.offer(num);
+            pq.offer(num);
 
-            if(minHeap.size() >k){
-                minHeap.poll();
+            if(pq.size() > k){
+                pq.poll();
             }
         }
     }
     
     public int add(int val) {
-        minHeap.offer(val);
-
-        if(minHeap.size() >k){
-            minHeap.poll();
-        }
-
-        return minHeap.peek();
+        pq.offer(val);
+       while(pq.size() >k){
+            pq.remove();  
+       }
+       return pq.peek();
     }
 }
 
