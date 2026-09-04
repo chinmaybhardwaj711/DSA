4a8a45a9cdf6009abe106e51ae646dcc9ed50ef0
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-04 09:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (62.63%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,92 +1,129 @@
-// class Solution {
-//     public List<Integer> spiralOrder(int[][] matrix) {
-//         int sr=0;
-//         int er=matrix.length-1;
-//         int sc=0;
-//         int ec=matrix[0].length-1;
-//         List<Integer> ans = new ArrayList<>();
+// // class Solution {
+// //     public List<Integer> spiralOrder(int[][] matrix) {
+// //         int sr=0;
+// //         int er=matrix.length-1;
+// //         int sc=0;
+// //         int ec=matrix[0].length-1;
+// //         List<Integer> ans = new ArrayList<>();
 
-//         while(sr<=er && sc<=ec){
+// //         while(sr<=er && sc<=ec){
 
-//             //right
-//             for(int i=sc;i<=ec;i++){
-//                 ans.add(matrix[sr][i]); 
-//             }
+// //             //right
+// //             for(int i=sc;i<=ec;i++){
+// //                 ans.add(matrix[sr][i]); 
+// //             }
            
-//             //down
-//                for(int j=sr+1;j<=er;j++){
-//                 ans.add(matrix[j][ec]);
-//             }
+// //             //down
+// //                for(int j=sr+1;j<=er;j++){
+// //                 ans.add(matrix[j][ec]);
+// //             }
            
-//             //left
+// //             //left
             
-//                 for(int k=ec-1;k>=sc;k--){
-//                     if(sr==er){
-//                         break;
-//                     }
-//                     ans.add(matrix[er][k]);
-//                 }
+// //                 for(int k=ec-1;k>=sc;k--){
+// //                     if(sr==er){
+// //                         break;
+// //                     }
+// //                     ans.add(matrix[er][k]);
+// //                 }
                
            
 
-//             //up
+// //             //up
             
-//                    for(int l=er-1;l>=sr+1;l--){
-//                     if(sc==ec){
-//                         break;
-//                     }
-//                      ans.add(matrix[l][sc]);
-//                     }
+// //                    for(int l=er-1;l>=sr+1;l--){
+// //                     if(sc==ec){
+// //                         break;
+// //                     }
+// //                      ans.add(matrix[l][sc]);
+// //                     }
                     
-//         sr++;
-//         er--;
-//         sc++;
-//         ec--;
+// //         sr++;
+// //         er--;
+// //         sc++;
+// //         ec--;
 
 
+// //         }
+
+// //         return ans;
+// //     }
+// // }
+// class Solution {
+//     public List<Integer> spiralOrder(int[][] matrix) {
+//         int sr=0;
+//         int sc=0;
+//         int er = matrix.length-1;
+//         int ec = matrix[0].length-1;
+//         List<Integer> ans = new ArrayList<>();
+
+//         while(sr<=er && sc<=ec){
+//             //right
+//             for(int col=sc;col<=ec;col++){
+//                 ans.add(matrix[sr][col]);
+//             }
+
+//             for(int row=sr+1;row<=er;row++){
+//                 ans.add(matrix[row][ec]);
+//             }
+
+//             for(int col=ec-1;col>=sc;col--){
+//                 if(sr==er){
+//                     break;
+//                 }
+//                 ans.add(matrix[er][col]);
+//             }
+
+//             for(int row=er-1;row>=sr+1;row--){
+//                 if(sc==ec){
+//                     break;
+//                 }
+//                 ans.add(matrix[row][sc]);
+//             }
+
+//             sr++;
+//             sc++;
+//             er--;
+//             ec--;
 //         }
-
 //         return ans;
+
 //     }
 // }
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int sr=0;
-        int sc=0;
+        int sr = 0;
+        int sc =0;
         int er = matrix.length-1;
         int ec = matrix[0].length-1;
         List<Integer> ans = new ArrayList<>();
-
         while(sr<=er && sc<=ec){
-            //right
+            //left to right
             for(int col=sc;col<=ec;col++){
                 ans.add(matrix[sr][col]);
             }
-
             for(int row=sr+1;row<=er;row++){
                 ans.add(matrix[row][ec]);
             }
-
             for(int col=ec-1;col>=sc;col--){
                 if(sr==er){
                     break;
                 }
                 ans.add(matrix[er][col]);
             }
-
             for(int row=er-1;row>=sr+1;row--){
                 if(sc==ec){
                     break;
                 }
                 ans.add(matrix[row][sc]);
+        
             }
 
-            sr++;
-            sc++;
-            er--;
-            ec--;
+            sr++;er--;
+            sc++;ec--;
         }
-        return ans;
 
+        return ans;
+        
     }
 }
