920dfd298b47bdbf324dd312774357022f77ceba
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-09 19:37:56 +0530

    Time: 5 ms (82.96%) | Memory: 77.3 MB (49.05%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,19 +1,38 @@
+// class Solution {
+//     public int maxArea(int[] height) {
+//       int lp =0;
+//       int maxArea =0;
+//       int  rp = height.length-1;
+//       while(lp<rp){
+//         int wdth = rp-lp;
+//         int ht = Math.min(height[lp],height[rp]);
+//         int currArea = ht*wdth;
+//          maxArea =Math.max(currArea,maxArea);
+//        if(height[lp] <height[rp]){
+//         lp++;
+//        }else{
+//          rp--;
+//        }
+//       }
+//       return maxArea;
+//     }
+// }
 class Solution {
     public int maxArea(int[] height) {
-      int lp =0;
-      int maxArea =0;
-      int  rp = height.length-1;
-      while(lp<rp){
-        int wdth = rp-lp;
-        int ht = Math.min(height[lp],height[rp]);
-        int currArea = ht*wdth;
-         maxArea =Math.max(currArea,maxArea);
-       if(height[lp] <height[rp]){
-        lp++;
-       }else{
-         rp--;
-       }
-      }
-      return maxArea;
+        int lp = 0;
+        int rp = height.length-1;
+        int maxArea = 0;
+        while(lp<rp){
+            int wd = rp-lp;
+            int ht = Math.min(height[lp],height[rp]);
+            int area = wd*ht;
+            maxArea = Math.max(area,maxArea);
+            if(height[lp] <height[rp]){
+                lp++;
+            }else{
+                rp--;
+            }
+        }
+        return maxArea;
     }
 }
