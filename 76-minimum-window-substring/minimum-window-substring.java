2ee9da645023a36c7f4fe33ccb638bd37ade1ad5
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-09 12:19:50 +0530

    Time: 2 ms (99.74%) | Memory: 45.7 MB (65.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,45 +1,88 @@
+// class Solution {
+//     public String minWindow(String s, String t) {
+//         if(s.length() < t.length()){
+//             return "";
+//         }
+
+//         int freq[] = new int[128];
+
+//         for(int i=0;i<t.length();i++){
+//             freq[t.charAt(i)]++;
+//         }
+//         int left =0;
+//         int count =t.length();
+
+//         int minLen = Integer.MAX_VALUE;
+//         int start =0;
+//         for(int right =0;right<s.length();right++){
+//             char ch = s.charAt(right);
+//            if(freq[ch] >0){
+//             count--;
+//            }
+//            freq[ch]--;
+
+//           while(count ==0){
+//             int len = right - left+1;
+//             if(len <minLen){
+//                 minLen = len;
+//                 start = left;
+//             }
+//             char leftChar = s.charAt(left);
+
+//             freq[leftChar]++;
+//             if(freq[leftChar] >0){
+//                 count++;
+//             }
+//             left++;
+
+
+//           }
+//         }
+
+//         return minLen == Integer.MAX_VALUE? "": s.substring(start,start+minLen);
+
+//         }
+// }
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() < t.length()){
+        if(s.length() <t.length()){
             return "";
         }
-
         int freq[] = new int[128];
 
         for(int i=0;i<t.length();i++){
             freq[t.charAt(i)]++;
         }
-        int left =0;
-        int count =t.length();
 
-        int minLen = Integer.MAX_VALUE;
+        int count =0;
         int start =0;
-        for(int right =0;right<s.length();right++){
-            char ch = s.charAt(right);
-           if(freq[ch] >0){
-            count--;
-           }
-           freq[ch]--;
+        int minLen = Integer.MAX_VALUE;
 
-          while(count ==0){
-            int len = right - left+1;
-            if(len <minLen){
-                minLen = len;
-                start = left;
-            }
-            char leftChar = s.charAt(left);
-
-            freq[leftChar]++;
-            if(freq[leftChar] >0){
+        int left =0;
+        for(int right=0;right<s.length();right++){
+            char c = s.charAt(right);
+            if(freq[c] >0){
                 count++;
             }
-            left++;
+            freq[c]--;
 
+            while(count == t.length()){
+                if(right-left+1<minLen){
+                    minLen = right-left+1;
+                    start = left;
+                }
+                char remove = s.charAt(left);
+                freq[remove]++;
+                if(freq[remove] >0){
+                    count--;
+                }
+                left++;
 
-          }
+            }
         }
-
-        return minLen == Integer.MAX_VALUE? "": s.substring(start,start+minLen);
-
+        if(minLen == Integer.MAX_VALUE){
+            return "";
         }
+        return s.substring(start,start+minLen);
+    }
 }
