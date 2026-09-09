9d5024c3d1ffe81fb15f95b701199d8d145e8525
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-09 13:59:04 +0530

    Time: 57 ms (38.35%) | Memory: 44.2 MB (44.58%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,25 +1,50 @@
-class Solution {
+// class Solution {
     
-    public int solve(String s, int start, int end,int k){
-        if(start>end){
+//     public int solve(String s, int start, int end,int k){
+//         if(start>end){
+//             return 0;
+//         }
+//         int freq[] = new int[26];
+//         for(int i=start;i<=end;i++){
+//             freq[s.charAt(i) - 'a']++;
+//         }
+//         for(int i=start;i<=end;i++){
+//             int idx = s.charAt(i)-'a';
+//             if(freq[idx] <k){
+//                 int left = solve(s,start,i-1,k);
+//                 int right = solve(s,i+1,end,k);
+//                   return Math.max(left,right);
+//             }
+          
+//         }
+
+//     return end-start+1;
+        
+//     }
+//     public int longestSubstring(String s, int k) {
+//         return solve(s,0,s.length()-1,k);
+//     }
+// }
+
+
+class Solution {
+    public int solve(String s, int start, int end, int k){
+        if(start >end){
             return 0;
         }
         int freq[] = new int[26];
         for(int i=start;i<=end;i++){
-            freq[s.charAt(i) - 'a']++;
+            freq[s.charAt(i)-'a']++;
         }
-        for(int i=start;i<=end;i++){
-            int idx = s.charAt(i)-'a';
-            if(freq[idx] <k){
+        for(int i =start;i<=end;i++){
+            if(freq[s.charAt(i)-'a'] <k){
                 int left = solve(s,start,i-1,k);
                 int right = solve(s,i+1,end,k);
-                  return Math.max(left,right);
-            }
-          
-        }
 
-    return end-start+1;
-        
+                return Math.max(left,right);
+            }
+        }
+        return end-start+1;
     }
     public int longestSubstring(String s, int k) {
         return solve(s,0,s.length()-1,k);
