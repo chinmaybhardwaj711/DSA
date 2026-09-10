6d957fa0341ebf0305657108967fc96aeb89b1bc
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-10 08:26:35 +0530

    Time: 2 ms (99.73%) | Memory: 45.4 MB (88.61%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,83 +1,161 @@
+// // class Solution {
+// //     public String minWindow(String s, String t) {
+// //         if(s.length() < t.length()){
+// //             return "";
+// //         }
+
+// //         int freq[] = new int[128];
+
+// //         for(int i=0;i<t.length();i++){
+// //             freq[t.charAt(i)]++;
+// //         }
+// //         int left =0;
+// //         int count =t.length();
+
+// //         int minLen = Integer.MAX_VALUE;
+// //         int start =0;
+// //         for(int right =0;right<s.length();right++){
+// //             char ch = s.charAt(right);
+// //            if(freq[ch] >0){
+// //             count--;
+// //            }
+// //            freq[ch]--;
+
+// //           while(count ==0){
+// //             int len = right - left+1;
+// //             if(len <minLen){
+// //                 minLen = len;
+// //                 start = left;
+// //             }
+// //             char leftChar = s.charAt(left);
+
+// //             freq[leftChar]++;
+// //             if(freq[leftChar] >0){
+// //                 count++;
+// //             }
+// //             left++;
+
+
+// //           }
+// //         }
+
+// //         return minLen == Integer.MAX_VALUE? "": s.substring(start,start+minLen);
+
+// //         }
+// // }
 // class Solution {
 //     public String minWindow(String s, String t) {
-//         if(s.length() < t.length()){
+//         if(s.length() <t.length()){
 //             return "";
 //         }
-
 //         int freq[] = new int[128];
 
 //         for(int i=0;i<t.length();i++){
 //             freq[t.charAt(i)]++;
 //         }
-//         int left =0;
-//         int count =t.length();
 
-//         int minLen = Integer.MAX_VALUE;
+//         int count =0;
 //         int start =0;
-//         for(int right =0;right<s.length();right++){
-//             char ch = s.charAt(right);
-//            if(freq[ch] >0){
-//             count--;
-//            }
-//            freq[ch]--;
+//         int minLen = Integer.MAX_VALUE;
 
-//           while(count ==0){
-//             int len = right - left+1;
-//             if(len <minLen){
-//                 minLen = len;
-//                 start = left;
-//             }
-//             char leftChar = s.charAt(left);
-
-//             freq[leftChar]++;
-//             if(freq[leftChar] >0){
+//         int left =0;
+//         for(int right=0;right<s.length();right++){
+//             char c = s.charAt(right);
+//             if(freq[c] >0){
 //                 count++;
 //             }
-//             left++;
+//             freq[c]--;
 
+//             while(count == t.length()){
+//                 if(right-left+1<minLen){
+//                     minLen = right-left+1;
+//                     start = left;
+//                 }
+//                 char remove = s.charAt(left);
+//                 freq[remove]++;
+//                 if(freq[remove] >0){
+//                     count--;
+//                 }
+//                 left++;
 
-//           }
+//             }
+//         }
+//         if(minLen == Integer.MAX_VALUE){
+//             return "";
+//         }
+//         return s.substring(start,start+minLen);
+//     }
+// }
+// class Solution {
+//     public String minWindow(String s, String t) {
+//         int freq[] = new int[128];
+
+//         for(int i=0;i<t.length();i++){
+//             freq[s.charAt(i)]++;
 //         }
 
-//         return minLen == Integer.MAX_VALUE? "": s.substring(start,start+minLen);
+//         int count =0;
+//         int start =0;
+//         int minLen = Integer.MAX_VALUE;
 
+//         int left =0
+//         for(int right=0;right<s.length();right++){
+//             char c = s.charAt(right);
+//             if(freq[c] >0){
+//                 count++;
+//             }
+//             freq[c]--;
+
+//             if(count == t.length()){
+//                 if(right-left+1<minLen){
+//                     minLen = right-left+1;
+//                     start = left;
+//                 }
+//                 char remove = s.charAt(left);
+//                 freq[remove]++;
+//                 if(freq[remove] >0){
+//                     count--;
+//                 }
+//                 left++;
+
+//             }
 //         }
+//         if(minLen = Integer.MAX_VALUE){
+//             return "";
+//         }
+//         return s.substring(start,start+minLen);
+//     }
 // }
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() <t.length()){
-            return "";
-        }
         int freq[] = new int[128];
-
         for(int i=0;i<t.length();i++){
             freq[t.charAt(i)]++;
         }
 
-        int count =0;
-        int start =0;
-        int minLen = Integer.MAX_VALUE;
 
         int left =0;
+        int start =0;
+        int cnt=0;
+        int minLen =Integer.MAX_VALUE;
         for(int right=0;right<s.length();right++){
-            char c = s.charAt(right);
-            if(freq[c] >0){
-                count++;
+            char ch = s.charAt(right);
+            if(freq[ch] >0){
+                cnt++;
             }
-            freq[c]--;
+            freq[ch]--;
 
-            while(count == t.length()){
+            while(cnt ==t.length()){
                 if(right-left+1<minLen){
                     minLen = right-left+1;
-                    start = left;
+                    start =left;
                 }
                 char remove = s.charAt(left);
                 freq[remove]++;
                 if(freq[remove] >0){
-                    count--;
+                    cnt--;
                 }
                 left++;
-
             }
         }
         if(minLen == Integer.MAX_VALUE){
@@ -85,4 +163,4 @@ class Solution {
         }
         return s.substring(start,start+minLen);
     }
-}
+}    
