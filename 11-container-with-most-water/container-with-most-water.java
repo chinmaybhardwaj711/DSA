90a8c067c25e444b0f9f0ea69f8384c1970a42d7
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-10 07:20:11 +0530

    Time: 5 ms (82.94%) | Memory: 77.3 MB (49.07%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,32 +1,17 @@
-// class Solution {
-//     public int maxArea(int[] height) {
-//       int lp =0;
-//       int maxArea =0;
-//       int  rp = height.length-1;
-//       while(lp<rp){
-//         int wdth = rp-lp;
-//         int ht = Math.min(height[lp],height[rp]);
-//         int currArea = ht*wdth;
-//          maxArea =Math.max(currArea,maxArea);
-//        if(height[lp] <height[rp]){
-//         lp++;
-//        }else{
-//          rp--;
-//        }
-//       }
-//       return maxArea;
-//     }
-// }
 class Solution {
     public int maxArea(int[] height) {
-        int lp = 0;
+        int lp =0;
         int rp = height.length-1;
-        int maxArea = 0;
+        int area =0;
+        int leftMax =0;
+        int rightMax =0;
+        int maxArea =0;
         while(lp<rp){
-            int wd = rp-lp;
+            int width = rp-lp;
             int ht = Math.min(height[lp],height[rp]);
-            int area = wd*ht;
-            maxArea = Math.max(area,maxArea);
+             area = ht*width;
+
+            maxArea= Math.max(area,maxArea);
             if(height[lp] <height[rp]){
                 lp++;
             }else{
