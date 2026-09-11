f91c3a3c6f9625877044df040985cce442540514
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-11 12:26:18 +0530

    Time: 6 ms (93.56%) | Memory: 44.9 MB (60.66%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,26 +1,48 @@
+// class Solution {
+//     public String largestNumber(int[] nums) {
+//         int n = nums.length;
+//         String arr[] = new String[n];
+
+//         for(int i=0;i<n;i++){
+//             arr[i] = String.valueOf(nums[i]);
+//         }
+//         Arrays.sort(arr,(a,b)->{
+//             String ab = a+b;
+//             String ba = b+a;
+//             return ba.compareTo(ab);
+//         }
+//             );
+
+//         if(arr[0].equals("0")){
+//             return "0";
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+//         for(String s:arr){
+//             sb.append(s);
+//         }
+//         return sb.toString();
+//     }
+// }
 class Solution {
     public String largestNumber(int[] nums) {
         int n = nums.length;
         String arr[] = new String[n];
 
         for(int i=0;i<n;i++){
-            arr[i] = String.valueOf(nums[i]);
+           arr[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(arr,(a,b)->{
-            String ab = a+b;
-            String ba = b+a;
-            return ba.compareTo(ab);
-        }
-            );
+
+        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));
 
         if(arr[0].equals("0")){
             return "0";
         }
-
         StringBuilder sb = new StringBuilder();
-        for(String s:arr){
-            sb.append(s);
+        for(String ar:arr){
+            sb.append(ar);
         }
+
         return sb.toString();
     }
 }
