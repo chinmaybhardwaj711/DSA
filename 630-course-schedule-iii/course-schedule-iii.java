1ec28db6305dfde01aeb17f0c63b42e0b4d3f07b
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-11 16:24:54 +0530

    Time: 38 ms (69.94%) | Memory: 57.4 MB (58.99%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.java
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -1,27 +1,52 @@
+// class Solution {
+//     public int scheduleCourse(int[][] courses) {
+//         Arrays.sort(courses,(a,b)->Integer.compare(a[1],b[1]));
+
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Integer.compare(b,a));
+
+//         int i=0;
+//         int n = courses.length;
+//         int time =0;
+//         while(i<n){
+//             int deadline = courses[i][1];
+//             int duration = courses[i][0];
+          
+//             time+=duration;
+//             pq.offer(courses[i][0]);
+//             i++;
+
+//               if(time > deadline){
+//                int remove = pq.poll();
+//                 time -=remove;
+
+//             }
+//         }
+//         return pq.size();
+
+//     }
+// }
+
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses,(a,b)->Integer.compare(a[1],b[1]));
+        Arrays.sort(courses,(a,b)->a[1]-b[1]);
+        int n = courses.length;
 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Integer.compare(b,a));
 
-        int i=0;
-        int n = courses.length;
         int time =0;
+        int i=0;
         while(i<n){
             int deadline = courses[i][1];
             int duration = courses[i][0];
-          
+            pq.offer(duration);
+
             time+=duration;
-            pq.offer(courses[i][0]);
-            i++;
-
-              if(time > deadline){
-               int remove = pq.poll();
-                time -=remove;
-
+            if(time>deadline){
+               int longest = pq.poll();
+               time-=longest;
             }
+            i++;
         }
         return pq.size();
-
     }
 }
