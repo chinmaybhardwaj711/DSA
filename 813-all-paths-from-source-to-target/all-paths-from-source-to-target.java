e0c08a8ad218e5ebcc91aa963e8af73811719d7a
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-13 14:17:16 +0530

    Time: 2 ms (90.47%) | Memory: 47.8 MB (75.53%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,24 +1,52 @@
-class Solution {
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<Integer> path = new ArrayList<>();
+// class Solution {
+//     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+//         List<Integer> path = new ArrayList<>();
 
-        List<List<Integer>> ans = new ArrayList<>();
+//         List<List<Integer>> ans = new ArrayList<>();
       
-        int n = graph.length;
-        dfs(graph,0,n-1,path,ans);
-        return ans;
-    }
-    public void dfs(int graph[][],int src,int dst, List<Integer> path, List<List<Integer>> ans){
-          path.add(src);
-        if(src == dst){
+//         int n = graph.length;
+//         dfs(graph,0,n-1,path,ans);
+//         return ans;
+//     }
+//     public void dfs(int graph[][],int src,int dst, List<Integer> path, List<List<Integer>> ans){
+//           path.add(src);
+//         if(src == dst){
+//             ans.add(new ArrayList<>(path));
+//             return;
+//         }
+//         for(int neigh:graph[src]){
+          
+//             dfs(graph,neigh,dst,path,ans);
+//             path.remove(path.size()-1);
+//         }
+        
+//     }
+
+
+// }
+
+
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public void dfs(int graph[][],int src,int target,List<Integer> path){
+        path.add(src);
+        if(src == target){
             ans.add(new ArrayList<>(path));
             return;
         }
         for(int neigh:graph[src]){
-          
-            dfs(graph,neigh,dst,path,ans);
+            dfs(graph,neigh,target,path);
             path.remove(path.size()-1);
         }
-        
+
+
+    }
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        // List<List<Integer>> ans = new ArrayList<>();
+        int n = graph.length;
+        List<Integer> path = new ArrayList<>();
+        dfs(graph,0,n-1,path);
+        return ans;
+
     }
 }
