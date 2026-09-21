976056355a77041e0a20559e3986c7b7510e860f
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-21 11:55:48 +0530

    Time: 116 ms (19.44%) | Memory: 48.8 MB (29.98%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,43 +1,88 @@
+// class Solution {
+//     class Pair{
+//         String first;
+//         int second;
+
+//         Pair(String first,int second){
+//             this.first = first;
+//             this.second = second;
+//         }
+//     }
+//     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//         Queue<Pair> q = new LinkedList<>();
+//         Set<String> s = new HashSet<String>();
+//         q.add(new Pair(beginWord,1));
+
+//         for(int i=0;i<wordList.size();i++){
+//             s.add(wordList.get(i));
+//         }
+//         s.remove(beginWord);
+
+//         while(!q.isEmpty()){
+//             String word = q.peek().first;
+//             int steps =q.peek().second;
+//             q.remove();
+
+//             if(word.equals(endWord)) return steps;
+//             //word = hat
+//             for(int i=0;i<word.length();i++){
+//                 for(char ch='a';ch<='z';ch++){
+//                     char[]charWord = word.toCharArray();
+//                     charWord[i] = ch;
+//                     String replacedWord = new String(charWord);
+//                     if(s.contains(replacedWord)){
+//                         s.remove(replacedWord); // mark visited
+//                         q.add(new Pair(replacedWord,steps+1));
+//                     }
+//                 }
+//             }
+//         }
+
+//         return 0;
+//     }
+// }
 class Solution {
-    class Pair{
-        String first;
-        int second;
-
-        Pair(String first,int second){
-            this.first = first;
-            this.second = second;
-        }
-    }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Pair> q = new LinkedList<>();
-        Set<String> s = new HashSet<String>();
-        q.add(new Pair(beginWord,1));
+        Set<String> set = new HashSet<>(wordList);
 
-        for(int i=0;i<wordList.size();i++){
-            s.add(wordList.get(i));
+        Queue<String> q = new LinkedList<>();
+
+        if(!set.contains(endWord)){
+            return 0;
         }
-        s.remove(beginWord);
 
+        q.offer(beginWord);
+
+        Set<String> vis = new HashSet<>();
+        vis.add(beginWord);
+        int level =1;
         while(!q.isEmpty()){
-            String word = q.peek().first;
-            int steps =q.peek().second;
-            q.remove();
+            int size = q.size();
 
-            if(word.equals(endWord)) return steps;
-            //word = hat
-            for(int i=0;i<word.length();i++){
-                for(char ch='a';ch<='z';ch++){
-                    char[]charWord = word.toCharArray();
-                    charWord[i] = ch;
-                    String replacedWord = new String(charWord);
-                    if(s.contains(replacedWord)){
-                        s.remove(replacedWord); // mark visited
-                        q.add(new Pair(replacedWord,steps+1));
+            for(int i=0;i<size;i++){
+                String word = q.poll();
+                if(word.equals(endWord)){
+                    return level;
+                }
+
+                for(int j=0;j<word.length();j++){
+                    for(char ch='a';ch<='z';ch++){
+                        StringBuilder sb = new StringBuilder(word);
+                        sb.setCharAt(j,ch);
+                        String newWord = sb.toString();
+
+                        if(set.contains(newWord)  && !vis.contains(newWord)){
+                            q.offer(newWord);
+                            vis.add(newWord);
+                        }
                     }
                 }
             }
+            level++;
         }
 
         return 0;
+
+
     }
 }
