e59f710e16bf407032c7d86011dfd1c3b6a08e9e
CHINMAY711 <138678293+chinmaybhardwaj711@users.noreply.github.com>
2026-09-22 13:46:08 +0530

    Time: 130 ms (71.06%) | Memory: 43 MB (43.73%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,85 +1,124 @@
-// class Solution {
-//     public  static boolean exist(char[][] board, String word) {
-//         int m = board.length; int n = board[0].length;
-//       for(int i=0;i<m;i++){
-//         for(int j=0;j<n;j++){
-//             if(board[i][j] == word.charAt(0)){
-//                 if(dfs(board,word,i,j,0)){
-//                     return true;
-//                 }
+// // class Solution {
+// //     public  static boolean exist(char[][] board, String word) {
+// //         int m = board.length; int n = board[0].length;
+// //       for(int i=0;i<m;i++){
+// //         for(int j=0;j<n;j++){
+// //             if(board[i][j] == word.charAt(0)){
+// //                 if(dfs(board,word,i,j,0)){
+// //                     return true;
+// //                 }
                 
-//             }
+// //             }
            
+// //         }
+
+// //       }
+// //       return false;
+// //     }
+
+// //     public  static boolean dfs(char[][]board,String word, int i,int j, int index ){
+// //         if(index == word.length())return true;
+// //         if(i>=board.length || i<0 ||j<0 ||j>=board[0].length)return false;
+// //         if(board[i][j] != word.charAt(index))return false;
+
+// //         char temp = board[i][j];
+
+// //         board[i][j] = '#';
+
+// //         boolean found = dfs(board,word,i+1,j,index+1) ||
+// //                         dfs(board,word,i-1,j,index+1) ||
+// //                         dfs(board,word,i,j+1,index+1) ||
+// //                         dfs(board,word,i,j-1,index+1) ;
+
+
+// //         board[i][j] = temp;
+// //         return found;
+// //     }
+// // }
+
+
+// class Solution {
+    
+//     public boolean dfs(char[][] board, String word,int i,int j,int index){
+//          int m = board.length;
+//         int n = board[0].length;
+//         if(i<0 || j<0 || i>=m || j>=n){
+//             return false;
 //         }
-
-//       }
-//       return false;
-//     }
-
-//     public  static boolean dfs(char[][]board,String word, int i,int j, int index ){
-//         if(index == word.length())return true;
-//         if(i>=board.length || i<0 ||j<0 ||j>=board[0].length)return false;
-//         if(board[i][j] != word.charAt(index))return false;
-
+      
+//         if(word.charAt(index) != board[i][j]){
+//             return false;
+//         }
+//           if(index == word.length()-1){
+//             return true;
+//         }
+//         if(board[i][j] == 'X'){
+//             return false;
+//         }
+//         index++;
 //         char temp = board[i][j];
+//         board[i][j] = 'X';
 
-//         board[i][j] = '#';
-
-//         boolean found = dfs(board,word,i+1,j,index+1) ||
-//                         dfs(board,word,i-1,j,index+1) ||
-//                         dfs(board,word,i,j+1,index+1) ||
-//                         dfs(board,word,i,j-1,index+1) ;
-
-
+     
+//            boolean found =  dfs(board,word,i+1,j,index) ||  dfs(board,word,i-1,j,index)||dfs(board,word,i,j+1,index) ||  dfs(board,word,i,j-1,index);
+        
 //         board[i][j] = temp;
 //         return found;
 //     }
+//     public boolean exist(char[][] board, String word) {
+//         int m = board.length;
+//         int n = board[0].length;
+//         int index =0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 char ch = word.charAt(index);
+//                 if(board[i][j] ==ch){
+//                     if(dfs(board,word,i,j,index)){
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+
+//     }
 // }
-
-
 class Solution {
-    
-    public boolean dfs(char[][] board, String word,int i,int j,int index){
-         int m = board.length;
-        int n = board[0].length;
-        if(i<0 || j<0 || i>=m || j>=n){
+    public boolean dfs(char board[][], int row,int col,String word,int index){
+        if(row<0 || row>=board.length || col<0 || col>=board[0].length){
             return false;
         }
-      
-        if(word.charAt(index) != board[i][j]){
+        if(board[row][col] == 'X'){
+            return false ;
+        }
+       
+        if(board[row][col] !=word.charAt(index)){
             return false;
         }
-          if(index == word.length()-1){
+         if(index == word.length()-1){
             return true;
         }
-        if(board[i][j] == 'X'){
-            return false;
-        }
-        index++;
-        char temp = board[i][j];
-        board[i][j] = 'X';
-
-     
-           boolean found =  dfs(board,word,i+1,j,index) ||  dfs(board,word,i-1,j,index)||dfs(board,word,i,j+1,index) ||  dfs(board,word,i,j-1,index);
         
-        board[i][j] = temp;
-        return found;
+        char temp = board[row][col];
+        board[row][col] = 'X';
+        boolean found = dfs(board,row+1,col,word,index+1) || dfs(board,row-1,col,word,index+1) || dfs(board,row,col+1,word,index+1) || dfs(board,row,col-1,word,index+1);
+        board[row][col] =  temp;
+        return  found;
+        
+
     }
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
         int index =0;
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                char ch = word.charAt(index);
-                if(board[i][j] ==ch){
-                    if(dfs(board,word,i,j,index)){
-                        return true;
-                    }
+                if(dfs(board,i,j,word,index)){
+                    return true;
                 }
             }
         }
         return false;
-
     }
 }
